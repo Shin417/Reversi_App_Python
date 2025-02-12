cd008acd0b64c074f3304d9f6f55453100264051
Shin417 <shintaroasoemail@gmail.com>
2025-02-11 19:17:41 -0500

    Connected frontend and backend and demonstrated

--- server/app.py
+++ server/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, origins='*') 
 
 @app.route("/")
 def main():
-    return
+    return "Hello"
 
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
